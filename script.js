a9ac9ba40b3baa4ae1de0bd279e52c00bac77d0b
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2024-04-12 11:35:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
     //Write your code here
-  
+  let row = prompt("Enter Row Number");
+  let col = prompt("Enter Col Number");
+  row = parseInt(row);
+  col = parseInt(col);
+  console.log(typeof row);
+  const table = document.getElementById("myTable");
+  for (let i = 0; i < row; i++) {
+    let row = table.insertRow();
+    for (let j = 0; j < col; j++) {
+      let cell = row.insertCell();
+      cell.textContent = "Row-" + (i) + ",Column-" + (j);
+    }
+  }
+  document.body.appendChild(table);
 }
